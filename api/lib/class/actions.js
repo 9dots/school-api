f90af6cf8149e7241a030ba5dc39f23d7ed088fd
danleavitt0 <danleavitt0@gmail.com>
2018-02-12 14:25:54 -0800

    add teacher

--- api/lib/class/actions.js
+++ api/lib/class/actions.js
@@ -3,7 +3,7 @@ const Module = require('../module')
 
 exports.removeStudent = (id, user) => Class.removeUser(id, user, 'student')
 exports.addStudent = (id, user) => Class.addUser(id, user, 'student')
-exports.createClass = (id, data) => Class.create(id, data)
+exports.createClass = Class.create
 // exports.addCourse = (id, course) => {
 //   Class
 // }
